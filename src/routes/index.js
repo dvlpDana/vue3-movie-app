245d004875744fdeec5f1e5167ce673b0aaf0403
colleksql3@gmail.com <simdanbi@simdanbiui-MacBookPro.local>
2022-09-19 19:24:09 +0900

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+
+export default createRouter({
+  // hash
+  // https://google.com/#/search
+  history : createWebHashHistory(),
+  // pages
+  // https://google.com/
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: About
+    },
+
+  ]
+})
